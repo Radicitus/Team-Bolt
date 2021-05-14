8509a7b5a2c02e9382c5d6422867142acc90d3de
Radicitus <novivolatus@gmail.com>
2021-05-13 23:44:19 -0700

    Added working alert system for when commands fail to send as well as accepting to send a command

--- Voice Control/app.py
+++ Voice Control/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, flash
 from flask import render_template
 from microphone_recognition import speech
 import requests
 
 app = Flask(__name__)
+app.secret_key = 'dev'
 
 team_bolt_raspi_url = "http://team-bolt-raspi.local:5000"
 command_dict = {
@@ -18,18 +19,30 @@ command_dict = {
     "bound": "kbd",
     "down": "d",
 }
-
+current_command = []
 
 @app.route('/')
 def root():
-    return render_template('app.html', text="")
+    return render_template('app.html', text="", commands=command_dict)
 
 
 @app.route('/speech')
 def begin_speech():
     text = speech()
     if text in command_dict:
-        comm_url = team_bolt_raspi_url + "/command/" + command_dict[text]
+        return render_template('app.html', text=text, command=command_dict[text])
+    else:
+        return render_template('app.html', text="commError", command=text.capitalize() + " is not a valid command!")
+
+
+@app.route('/begin-command/<command>')
+def begin_command(command):
+    comm_url = team_bolt_raspi_url + "/command/" + command_dict[command]
+    try:
         response = requests.post(comm_url)
-        print(response)
-    return render_template('app.html', text=text, command="TEST COMMAND")
+        flash("Command sent successfully!")
+        return redirect(url_for("root"))
+    except:
+        flash("Sending command failed.")
+        return redirect(url_for("root"))
+
